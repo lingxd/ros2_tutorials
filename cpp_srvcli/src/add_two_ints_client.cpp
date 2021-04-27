da9be77b9b14cb9b43cc5069784a2f6bfebb3fb1
hokori <lingxd97@outlook.com>
2021-04-27 11:11:36 +0800

    :sparkles:使用自己定义的.msg和.srv

--- cpp_srvcli/src/add_two_ints_client.cpp
+++ cpp_srvcli/src/add_two_ints_client.cpp
@@ -1,5 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
+#include "tutorial_interfaces/srv/add_three_ints.hpp"        // CHANGE
 
 #include <chrono>
 #include <cstdlib>
@@ -11,18 +11,19 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  if (argc != 3) {
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
+  if (argc != 4) { // CHANGE
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_three_ints_client X Y Z");      // CHANGE
       return 1;
   }
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-  rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client =
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_three_ints_client"); // CHANGE
+  rclcpp::Client<tutorial_interfaces::srv::AddThreeInts>::SharedPtr client =                        // CHANGE
+    node->create_client<tutorial_interfaces::srv::AddThreeInts>("add_three_ints");                  // CHANGE
 
-  auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+  auto request = std::make_shared<tutorial_interfaces::srv::AddThreeInts::Request>();               // CHANGE
   request->a = atoll(argv[1]);
   request->b = atoll(argv[2]);
+  request->c = atoll(argv[3]);               // CHANGE
 
   while (!client->wait_for_service(1s)) {
     if (!rclcpp::ok()) {
@@ -35,11 +36,11 @@ int main(int argc, char **argv)
   auto result = client->async_send_request(request);
   // Wait for the result.
   if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
+    rclcpp::executor::FutureReturnCode::SUCCESS)
   {
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
   } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_three_ints");    // CHANGE
   }
 
   rclcpp::shutdown();
--- cpp_srvcli/src/add_two_ints_server.cpp
+++ cpp_srvcli/src/add_two_ints_server.cpp
@@ -1,14 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
+#include "tutorial_interfaces/srv/add_three_ints.hpp"     // CHANGE
 
 #include <memory>
 
-void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>     response)
+void add(const std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Request> request,     // CHANGE
+          std::shared_ptr<tutorial_interfaces::srv::AddThreeInts::Response>       response)  // CHANGE
 {
-  response->sum = request->a + request->b;
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-                request->a, request->b);
+  response->sum = request->a + request->b + request->c;                                       // CHANGE
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld" " c: %ld",   // CHANGE
+                request->a, request->b, request->c);                                          // CHANGE
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
 }
 
@@ -16,12 +16,12 @@ int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_three_ints_server");  // CHANGE
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+  rclcpp::Service<tutorial_interfaces::srv::AddThreeInts>::SharedPtr service =                 // CHANGE
+    node->create_service<tutorial_interfaces::srv::AddThreeInts>("add_three_ints",  &add);     // CHANGE
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add three ints.");      // CHANGE
 
   rclcpp::spin(node);
   rclcpp::shutdown();
